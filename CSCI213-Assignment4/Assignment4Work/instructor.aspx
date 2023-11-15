af785aac63889389a880a67245a30074fef05f7c
Gunnar Moody <moodygman04@gmail.com>
2023-11-15 13:21:58 -0600

    fixed logon + base of instructor page
    fixed logon page:
    - added setauthcookie to keep track of who is logged in
    
    added base to instructor page:
    - welcomes user with full name

--- CSCI213-Assignment4/Assignment4Work/instructor.aspx
+++ CSCI213-Assignment4/Assignment4Work/instructor.aspx
@@ -1,3 +1,11 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="instructor.aspx.cs" Inherits="CSCI213_Assignment4.Assignment4Work.instructor" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        Welcome,
+        <asp:Label ID="lblFirstName" runat="server" Text="Label"></asp:Label>
+&nbsp;<asp:Label ID="lblLastName" runat="server" Text="Label"></asp:Label>
+        <br />
+    </p>
+    <p>
+    </p>
 </asp:Content>
--- CSCI213-Assignment4/Assignment4Work/instructor.aspx.cs
+++ CSCI213-Assignment4/Assignment4Work/instructor.aspx.cs
@@ -11,7 +11,45 @@ namespace CSCI213_Assignment4.Assignment4Work
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // get relative path to database
+            string connString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
 
+            // connect to database
+            using (var db = new KarateSchoolDataContextDataContext(connString))
+            {
+                try
+                {
+                    // get user id from database based on user name
+                    string userName = User.Identity.Name;
+                    lblLastName.Text = userName;
+
+                    var result1 = (from user in db.NetUsers
+                                   where user.UserName == userName
+                                   select user.UserID).First();
+
+                    int userID = Convert.ToInt32(result1);
+
+                    // get first and last name from database based on user id
+                    var result2 = (from instructor in db.Instructors
+                                   where instructor.InstructorID == userID
+                                   select instructor.InstructorFirstName).First();
+
+                    var result3 = (from instructor in db.Instructors
+                                   where instructor.InstructorID == userID
+                                   select instructor.InstructorLastName).First();
+
+                    string firstName = Convert.ToString(result2);
+                    string lastName = Convert.ToString(result3);
+
+                    // display first and last name
+                    lblFirstName.Text = firstName;
+                    lblLastName.Text = lastName;
+                }
+                catch (Exception)
+                {
+                    lblFirstName.Text = "error";
+                }
+            }
         }
     }
 }
